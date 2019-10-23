ce802512faa35c73480ad5251a6a0fc58f66970c
alumno <alumno@aula1-10>
2019-10-23 14:37:15 +0200

    ejercicios

---
+++ src/vectore/vectore5.java
@@ -0,0 +1,21 @@
+package vectore;
+
+import java.util.Scanner;
+
+public class vectore5 {
+
+	public static void main(String[] args) {
+		Scanner leer;
+		leer = new Scanner(System.in);
+		
+		int tamaño = 0;
+		int tamaño1=0;
+		
+		tamaño= leer.nextInt();
+		tamaño1=leer.nextInt();
+		
+		String nnombres[][]= new String[tamaño][tamaño1];
+	skele
+	}
+
+}
